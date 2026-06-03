bda4a11a53263ebc442c65e968fec4357e54ebe0
Zydie Morris A. Cansancio <cansanciozydie@gmail.com>
2026-06-03 15:27:03 +0800

    commit 1

--- resources/js/pages/Dashboard.vue
+++ resources/js/pages/Dashboard.vue
@@ -2,6 +2,7 @@
 import { Head } from '@inertiajs/vue3';
 import PlaceholderPattern from '@/components/PlaceholderPattern.vue';
 import { dashboard } from '@/routes';
+import Button from '@/components/ui/button/Button.vue';
 
 defineOptions({
     layout: {
@@ -22,5 +23,7 @@ defineOptions({
         class="flex h-full flex-1 flex-col gap-4 overflow-x-auto rounded-xl p-4"
     >
         Hello Dashboard 2
+
+        <Button>Hello</Button>
     </div>
 </template>
