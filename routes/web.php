d7872901b4aa14e170ab97aa1e7f7832d6d52f00
Zydie Morris A. Cansancio <cansanciozydie@gmail.com>
2026-06-03 13:52:07 +0800

    Dashboard Page

--- routes/web.php
+++ routes/web.php
@@ -6,6 +6,10 @@ Route::inertia('/', 'Welcome')->name('home');
 
 Route::middleware(['auth', 'verified'])->group(function () {
     Route::inertia('dashboard', 'Dashboard')->name('dashboard');
+    Route::inertia('projects', 'Projects')->name('projects');
+    Route::inertia('user', 'User')->name('user');
 });
+
+
 
 require __DIR__.'/settings.php';
